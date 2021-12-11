80e9cf3a4fe594ad7d85a254e22538f108f67f33
Edison Garcia <egachi@live.com>
2021-12-10 18:49:26 -0600

    Changing code

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'notverysecret!'
-socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'random!'
+socketio = SocketIO(app, cors_allowed_origins="*"))
 
 @app.route("/")
-def hello():
+def home():
     return render_template('index.html')
 
 @socketio.on("client_to_server")
 def client_to_server(msg):
     message = "pong"
-    print("message from server to client: " + message)
-    socketio.emit("server_to_client", {"msg": message})
+    print("Message from server to client: " + message)
+    socketio.emit("server_to_client", { "msg": message })
 
 if __name__ == '__main__':
-    socketio.run(app,port=8000, host='0.0.0.0', debug=True)
+    socketio.run(app, port=8000, host='0.0.0.0', debug=True)
     print('socket io start')
