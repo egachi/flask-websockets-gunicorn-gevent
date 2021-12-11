2ead1bf1a92d6b0d6e3f12de36f8d3ca00f81b94
Edison Garcia <egachi@live.com>
2021-12-10 18:05:15 -0600

    Changing code

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'notverysecret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app,cors_allowed_origins='*')
 
 @app.route("/")
 def hello():
